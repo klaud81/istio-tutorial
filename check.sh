fcd4f3b268ae364f4c9bf843c8f6024efa1c4b6e
claud kim <claud.kim81@gmail.com>
2019-04-04 15:11:07 +0900

    ubuntu minishift install guide

--- check.sh
+++ check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-modules="git docker curl java"
+modules="git docker curl java virtualbox minishift stern siege"
 #modules="git docker curl stern siege java"
 
 curl -sL https://git.io/_has | HAS_ALLOW_UNSAFE=y bash -s ${modules}
